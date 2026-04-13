c77aeddd97075329d9ee08da11a72ba3ac81703c
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-04-13 20:11:47 +0900

    [fix] 기본 모달 모듈 css 수정#19

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -10,18 +10,18 @@ export default function Modal({
   onConfirm,
 }) {
   return (
-    <section className={styles.modalBackground}>
-      <article className={styles.modalContainer}>
-        <header className={styles.modalHeader}>
+    <section className={styles['modalBackground']}>
+      <article className={styles['modalContainer']}>
+        <header className={styles['modalHeader']}>
           <h2>{title}</h2>
           {/* <h2>제목</h2> */}
         </header>
 
-        <main className={styles.modalContent}>
+        <main className={styles['modalContent']}>
           <p>{message}</p>
         </main>
 
-        <footer className={styles.modalActions}>
+        <footer className={styles['modalActions']}>
           <button onClick={onCancel}>{cancelText}</button>
           <button onClick={onConfirm}>{confirmText}</button>
         </footer>
