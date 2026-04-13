a548bed762d15108247aac6929ad2c2b5073d33f
biyuns <164608385+biyuns@users.noreply.github.com>
2026-04-13 22:39:28 +0900

    [hotfix] App.jsx 파일 병합 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "react-router-dom"
 import{router}from "./routes/router.jsx"
-
-function App() {
 import Header from './components/layout/Header';
 import Modal from './components/common/Modal';
 import ChkModal from './components/common/ChkModal';
