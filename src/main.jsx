4f9506477ae4c579ffe498cb879a19a70684ea43
biyuns <164608385+biyuns@users.noreply.github.com>
2026-04-13 22:45:17 +0900

    [hotfix] main.jsx 병합 오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,3 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <App />
 )
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(<App />);
