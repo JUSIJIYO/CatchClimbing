d8666e763c54004b34dbdf1ef31d09a319f2ecc1
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-04-13 17:38:11 +0900

    [feature] header 기능 구현 issue #9

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import logo from '../../assets/icon/climbing_logo.png';
+import loginIcon from '../../assets/icon/login.svg';
+import logoutIcon from '../../assets/icon/logout.svg';
+import title from '../../assets/icon/logo.png';
+import styles from '../../styles/css/layout/header.module.css';
 
 function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <div></div>
-  )
+    <>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <img src={logo} alt="logo" className={styles.logo} />
+          <img src={title} alt="title" className={styles.title} />
+        </div>
+
+        <div>
+          <button
+            className={`${styles.button} ${isLogin ? styles.logout : styles.login}`}
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            <img
+              src={isLogin ? logoutIcon : loginIcon}
+              alt="icon"
+              className={styles.icon}
+            />
+            {isLogin ? '로그아웃' : '로그인'}
+          </button>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
