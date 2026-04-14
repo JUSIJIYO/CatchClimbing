9b625330b9a93e44fd58f3124e78c0a9559f59df
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-04-15 00:35:45 +0900

    [feature] 지점 강사 상세조회 컴포넌트 구현#69

--- src/components/branch/BranchPrfDetail.jsx
+++ src/components/branch/BranchPrfDetail.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../styles/css/branch/BranchPrfDetail.module.css';
+import leftArrow from '../../assets/icon/leftarrow.svg';
+import rightArrow from '../../assets/icon/rightarrow.svg';
 
-function BranchProfessorDetail() {
+function BranchPrfDetail({
+  prf,
+  index,
+  prfList,
+  setSelectedIndex,
+  setSelectedPrf,
+}) {
+  const handlePrev = () => {
+    if (index > 0) {
+      const newIndex = index - 1;
+      setSelectedIndex(newIndex);
+      setSelectedPrf(prfList[newIndex]);
+    }
+  };
+
+  const handleNext = () => {
+    if (index < prfList.length - 1) {
+      const newIndex = index + 1;
+      setSelectedIndex(newIndex);
+      setSelectedPrf(prfList[newIndex]);
+    }
+  };
   return (
-    <div></div>
-  )
+    <div className={styles['wrapper']}>
+      {index > 0 && (
+        <img
+          src={leftArrow}
+          className={styles['arrowLeft']}
+          onClick={handlePrev}
+        />
+      )}
+      <div className={styles['card']}>
+        <div className={styles['top']}>
+          <img
+            src="/default-profile.png"
+            alt="프로필"
+            className={styles['profile']}
+          />
+
+          <div className={styles['info']}>
+            <h2>{prf.name}</h2>
+            <p>레벨: {prf.level}</p>
+            <p>소속: {prf.branch}</p>
+          </div>
+        </div>
+
+        <div className={styles['career']}>
+          <h3>대회경력</h3>
+          {prf.career?.map((c, index) => (
+            <p key={index}>{c}</p>
+          ))}
+        </div>
+      </div>
+      {index < prfList.length - 1 && (
+        <img
+          src={rightArrow}
+          className={styles['arrowRight']}
+          onClick={handleNext}
+        />
+      )}
+    </div>
+  );
 }
 
-export default BranchProfessorDetail
+export default BranchPrfDetail;
