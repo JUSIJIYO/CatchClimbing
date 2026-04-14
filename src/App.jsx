3383596b86c6bea229248c1a88bbc003df7ebd8c
biyuns <cart0089@naver.com>
2026-04-14 15:07:40 +0900

    [feature] 라우터 설정#43

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.jsx';
-// import{router}from "./routes/router.jsx"
-import Header from './components/layout/Header';
-import Modal from './components/common/Modal';
-import ChkModal from './components/common/ChkModal';
-import ConfirmModal from './components/common/ConfirmModal';
-import BranchListPage from './pages/branch/BranchListPage.jsx';
-import Nav from "./components/layout/Nav"
-import AuthLayout from "./layouts/AuthLayout.jsx";
 import '../src/styles/global.css'
-import FileUpload from "./components/common/FileUpload";
-
+import { router } from '../src/routes/router.jsx'
+import Header from './components/layout/Header.jsx';
 function App() {
   return (
     <>
-      <BranchListPage />
-      {/* <RouterProvider router={router}/> */}
-      {/* 
-      <Modal />
-      <ChkModal />
-      <ConfirmModal />
-       */}
-      <Header />
-      <AuthLayout />
-      <Nav />
-      <FileUpload />
+      <RouterProvider router={router}/>    
     </>
+    
   );
 }
 
