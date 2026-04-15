89aee92dfcaf5b1d76e0d8ad36d3a8f38fac2ac2
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2026-04-15 11:00:42 +0900

    [feature] 지점조회페이지 내 커뮤니티 아이템 컴포넌트 구현#75

--- src/components/branch/BranchCommuItem.jsx
+++ src/components/branch/BranchCommuItem.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import personIcon from '../../assets/icon/commuPerson.svg';
+import eyeIcon from '../../assets/icon/commuEye.svg';
+import commentIcon from '../../assets/icon/commuComment.svg';
+import styles from '../../styles/css/branch/BranchCommuItem.module.css';
 
-function BranchCommunityItme() {
+function BranchCommuItem({ post }) {
   return (
-    <div></div>
-  )
+    <div className={styles['card']}>
+      <div className={stylestop}>
+        <h3>{post.title}</h3>
+        <span className={styles.branch}>{post.branch}</span>
+      </div>
+
+      <div className={styles.author}>
+        <img src={personIcon} alt="" />
+        <span>{post.author}</span>
+      </div>
+
+      <div className={styles.meta}>
+        <div>
+          <img src={eyeIcon} alt="" />
+          <span>{post.views}</span>
+        </div>
+
+        <div>
+          <img src={commentIcon} alt="" />
+          <span>{post.comments}</span>
+        </div>
+
+        <span>{post.time}</span>
+      </div>
+    </div>
+  );
 }
 
-export default BranchCommunityItme
+export default BranchCommuItem;
