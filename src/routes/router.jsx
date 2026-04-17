97980727b58fb6caf48057357cc5fe03c9d28082
jia40 <lsc7439@g.eulji.ac.kr>
2026-04-17 13:15:03 +0900

    [feature]커뮤니티 메인페이지 구현#165

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,26 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../layouts/MainLayout';
-import DashBoardPage from '../pages/admin/DashBoardPage';
-import BranchDetailPage from '../pages/branch/BranchDetailPage';
-import AuthLayout from '../layouts/AuthLayout';
-import BranchListPage from '../pages/branch/BranchListPage';
-import LoginPage from '../pages/auth/LoginPage';
-import SignUpPage from '../pages/auth/SignUpPage';
-import StuSignUpPage from '../pages/auth/StuSignUpPage';
-import PrfSignUpPage from '../pages/auth/PrfSignUpPage';
-import SignUpForm from '../components/auth/SignUpForm';
-import CommuPostPage from '../pages/community/CommuPostPage';
-import ClassListPage from '../pages/class/ClassListPage';
-import ClassDetailPage from '../pages/class/ClassDetailPage';
-import Mypage from '../pages/mypage/Mypage';
-import SignUpCompletePage from '../pages/auth/SignUpCompletePage';
-import MyLevelSystemPage from '../pages/mypage/MyLevelSystemPage';
-import RecordListPage from '../pages/record/RecordListPage';
-import RecordFormPage from '../pages/record/RecordFormPage';
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import DashBoardPage from "../pages/admin/DashBoardPage";
+import BranchDetailPage from "../pages/branch/BranchDetailPage";
+import AuthLayout from "../layouts/AuthLayout";
+import BranchListPage from "../pages/branch/BranchListPage";
+import LoginPage from "../pages/auth/LoginPage";
+import SignUpPage from "../pages/auth/SignUpPage";
+import StuSignUpPage from "../pages/auth/StuSignUpPage";
+import PrfSignUpPage from "../pages/auth/PrfSignUpPage";
+import SignUpForm from "../components/auth/SignUpForm";
+import CommuPostPage from "../pages/community/CommuPostPage";
+import ClassListPage from "../pages/class/ClassListPage";
+import ClassDetailPage from "../pages/class/ClassDetailPage";
+import Mypage from "../pages/mypage/Mypage";
+import SignUpCompletePage from "../pages/auth/SignUpCompletePage";
+import MyLevelSystemPage from "../pages/mypage/MyLevelSystemPage";
+import RecordListPage from "../pages/record/RecordListPage";
+import RecordFormPage from "../pages/record/RecordFormPage";
+import CommuPage from "../pages/community/CommuPage";
+import CommuReviewPage from "../pages/community/CommuReviewPage";
+import PostDetailPage from "../pages/community/PostDetailpage";
+import ReviewDetailPage from "../pages/community/ReviewDetailPage";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
@@ -28,50 +32,65 @@ export const router = createBrowserRouter([
         element: <BranchListPage />,
       },
       {
-        path: 'branch/:id',
+        path: "branch/:id",
         element: <BranchDetailPage />,
       },
       {
-        path: 'community',
-        element: <CommuPostPage />,
+        path: "commu",
+        element: <CommuPage />,
       },
       {
-        path: 'class',
+        path: "community",
+        element: <CommuPostPage />,
+      },
+
+      {
+        path: "/review",
+        element: <CommuReviewPage />,
+      },
+
+      {
+        path: "/postdetail",
+        element: <PostDetailPage />,
+      },
+      {
+        path: "/reviewdetail",
+        element: <ReviewDetailPage />,
+      },
+      
+      {
+        path: "class",
         element: <ClassListPage />,
       },
       {
-        path: 'class/:id',
+        path: "class/:id",
         element: <ClassDetailPage />,
       },
       {
-        path: 'mypage',
+        path: "mypage",
         element: <Mypage />,
       },
       {
-        path: 'level',
+        path: "level",
         element: <MyLevelSystemPage />,
       },
       {
-        path: 'class/:id',
-        element: <ClassDetailPage />,
-      },
-      {
-        path: 'record',
+        path: "record",
         element: <RecordListPage />,
       },
       {
-        path: 'record/new',
+        path: "record/new",
         element: <RecordFormPage />,
       },
       {
         element: <AuthLayout />,
         children: [
           {
-            path: 'login',
+            path: "login",
             element: <LoginPage />,
           },
           {
-            path: 'signup',
+            path: "signup",
             element: <SignUpPage />,
             children: [
               {
@@ -79,17 +98,17 @@ export const router = createBrowserRouter([
                 element: <SignUpForm />,
               },
               {
-                path: 'stu',
+                path: "stu",
                 element: <StuSignUpPage />,
               },
               {
-                path: 'prf',
+                path: "prf",
                 element: <PrfSignUpPage />,
               },
             ],
           },
           {
-            path: 'signupcomplete',
+            path: "signupcomplete",
             element: <SignUpCompletePage />,
           },
         ],
@@ -97,11 +116,11 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/totaladmin',
+    path: "/totaladmin",
     element: <DashBoardPage />,
   },
   {
-    path: 'branchadmin',
+    path: "branchadmin",
     element: <DashBoardPage />,
   },
 ]);
